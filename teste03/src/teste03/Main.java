4fa90c5bafc8858d6e0899174028793890d0c89b
amandakicke <amandakicke@gmail.com>
2022-06-07 18:20:44 -0300

    commit final

--- teste03/src/teste03/Main.java
+++ teste03/src/teste03/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		IPI ipi = new IPI();
+		
+		ipi.valor(5000);
+		ipi.calculaImposto(500);
+		
+		Cofins cof = new Cofins();
+		
+		cof.calculaImposto();
 	}
 
 }
